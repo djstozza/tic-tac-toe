3229fef2ec7c77116ef6196f0d794f0ea61cfddd
djstozza <daniel.sztolcman@gmail.com>
2015-08-18 15:51:33 +1000

    add indexes of X and O to player1 and player 2 arrays respectively

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -17,12 +17,42 @@ var ticTacToe = {
   	player1: [],
   	player2: [],
 
-  	
+  	winningMove: function () {
+  		var win = [	[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [ 1, 4, 7], [2, 5, 8], [1, 4, 8], [2, 4, 6]	];
+ 
+  	},
 
+  	allIndexesOfX: function () {
+  		for (var i = 0; i < this.board.length; i++){
+  			if (this.board[i] === "X") {
+  				this.player1.push(i);
+  			}
+  			//removes all the duplicate elements from the player1 array
+  			$('#dvallElements').html(this.player1.join("<br />"));
+   	 		this.player1 = $.unique(this.player1);
+    		$('#dvUniqueItems').html(this.player1.sort().join("<br />"));
 
-
+  		}
+  	},
 	
 
+
+
+
+  	allIndexesOfO: function () {
+  		for (var i = 0; i < this.board.length; i++){
+  			if (this.board[i] === "O") {
+  				this.player2.push(i);
+  			}
+  			//removes all duplicate elements from the player2 array
+  			$('#dvallElements').html(this.player2.join("<br />"));
+   	 		this.player2 = $.unique(this.player2);
+    		$('#dvUniqueItems').html(this.player2.sort().join("<br />"));
+  		}
+  	},
+
+
+  	
 
 	
 
@@ -30,23 +60,30 @@ var ticTacToe = {
   	init: function () {
     // jQuery stuff goes here
    		var turnCount = 0;
-   			$('button').on('click', function(){
-    		var id = parseInt( $(this).attr('class') );
-    		//.attr()will find the attribute
-    		if (turnCount%2 === 0) {
-    			$(this).text('X');
-    			ticTacToe.board['class'] = "X";
-    		} else {
-    			$(this).text('O');
-    			ticTacToe.board['class'] = "O";
-    		}
-    		console.log( ticTacToe.board );
-    		turnCount++;
-    	});
-    	
+		$('button').on('click', function(){
+		var id = parseInt( $(this).attr('id') );
+		//.attr()will find the attribute
+		if (turnCount%2 === 0) {
+			$(this).text('X').addClass('X');
+			ticTacToe.board[id] = "X";
 
+			//$('button.X');
+		} else {
+			$(this).text('O').addClass('O');
+			ticTacToe.board[id] = "O";
+			$('button.O');
+			
+		}
+		//console.log(ticTacToe.player1.push($('button.X').attr('id')));
+		ticTacToe.allIndexesOfX();
+		ticTacToe.allIndexesOfO();
+		console.log(ticTacToe.player1);
+		console.log(ticTacToe.player2);
+		console.log( ticTacToe.board );
+		turnCount++;
 
-   		
+	    });
+
   	}
 }
 
