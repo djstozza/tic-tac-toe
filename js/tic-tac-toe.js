0dee9e8ef0263a7165d2c4c21de72e25c25fd781
djstozza <daniel.sztolcman@gmail.com>
2015-08-19 16:54:58 +1000

    add switch player function. bug when calling player2 as a winner on odd rounds

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -16,6 +16,9 @@ var ticTacToe = {
 
   	player1: [],
   	player2: [],
+    player1Wins: 0,
+    player2Wins: 0,
+    gameCount: 0,
 
   	
 
@@ -23,62 +26,95 @@ var ticTacToe = {
   	winningMove: function () {
   		var win = [	[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [ 1, 4, 7], [2, 5, 8], [1, 4, 8], [2, 4, 6]	];
         for (var i = 0; i < win.length; i++) {
-          if ( $( win[i] ).not( this.player1 ).length === 0 ) {
-            return alert( 'Player 1 Wins!' );
-          } else if ( $( win[i] ).not( this.player2 ).length === 0 ) {
-            return alert( 'Player 2 Wins!' );
+          
+          if ( $( win[i] ).not( this.player1 ).length === 0 && (this.player1.length >= 3)) {
+            
+            this.player1Wins++;
+            $('.player1').css('display', 'inline-block');
+            $('.playAgain').css('opacity', '1');
+            return;
+            
+          } else if ( $( win[i] ).not( this.player2 ).length === 0 && (this.player2.length >= 3) ) {
+            
+            this.player2Wins++;
+            $('.player2').css('display', 'inline-block');
+            $('.playAgain').css('opacity', '1');
+            return;
+
+          } else if ( ($( win[i] ).not( this.player1 ).length !== 0) && (this.player1.length === 5)) {
+            
+
+            $('.draw').css('display', 'inline-block');
+            $('.playAgain').css('opacity', '1');
+            return;
           }
+
         }
         
       },
 
-          // this won't work since  siince you can't compare arrays.
-          // if ($.inArray(win[i], this.player1) !== -1 && (this.player1.length >= 3) && $.inArray(win[i], this.player2) == -1) {
-          //   return alert('Player1 wins!');
-    isDraw: function () {
-      var win = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [ 1, 4, 7], [2, 5, 8], [1, 4, 8], [2, 4, 6] ];
-        for (var i = 0; i < win.length; i++) {
-          if ( ($( win[i] ).not( this.player1 ).length !== 0) && ($( win[i] ).not( this.player2 ).length !== 0 ) ) {
-            return alert( 'This is a Draw!' );
-          }
-        }
+         
+
+    clearBoard: function () {
+      $('button.cell').text('');
+      this.board = [ null, null, null, null, null, null, null, null, null ];
+      this.player1 = [];
+      this.player2 = [];
+      this.gameCount++;
+      console.log(this.player1Wins);
+      console.log(this.player2Wins);
+      console.log(this.gameCount);
+      if (this.gameCount%2 !== 0) {
+        this.player1 === this.player2;
+        this.player2 === this.player1;
+
+        
+        
+      }
     },
 
   	init: function () {
     // jQuery stuff goes here
       var turnCount = 0;
-      $('button').on('click', function(){
+      $('button.cell').on('click', function(){
       var id = parseInt( $(this).attr('id') );
     
     //stops a square from being re-assigned when it has already been clicked
     //.trim() to make sure there is no blank space in the square already
       if ($(this).text().trim() !== ''){
-        alert('You already have played here');
-        return;
+          alert('You already have played here');
+          return;
     }
+      //.attr()will find the attribute
+      if (turnCount%2 === 0) {
+        $(this).text('X').addClass('X');
+        ticTacToe.player1.push(id);
+        ticTacToe.player1.sort();
+        ticTacToe.board[id] = "X";
+        
+      } else {
+        
+        ticTacToe.player2.push(id);
+        ticTacToe.player2.sort();
+        $(this).text('O').addClass('O');
+        ticTacToe.board[id] = "O";
+      }
+      debugger;
+      ticTacToe.winningMove();
+      turnCount++;
+      // if (turnCount === 9) {
+      //   ticTacToe.isDraw();
+      // } 
+    
+    });
+    
+    $('button.yes').on('click', function () {
+      $('.playAgain').css('opacity', '0');
+      ticTacToe.clearBoard();
 
-    
-        //.attr()will find the attribute
-        if (turnCount%2 === 0) {
-          $(this).text('X').addClass('X');
-          ticTacToe.player1.push(id);
-          ticTacToe.player1.sort();
-          ticTacToe.board[id] = "X";
-          
-        } else {
-          ticTacToe.player2.push(id);
-          ticTacToe.player2.sort();
-          $(this).text('O').addClass('O');
-          ticTacToe.board[id] = "O";
-        }
-        ticTacToe.winningMove();
-        turnCount++;
-        if (turnCount === 9) {
-          ticTacToe.isDraw();
-        } 
-    
-      });
-    }
+      
+    })
+  }
 }
 
 // then a minimal document ready:
@@ -87,6 +123,19 @@ $(document).ready(function () {
     ticTacToe.init();
 });
 
+
+
+ // this won't work since  siince you can't compare arrays.
+          // if ($.inArray(win[i], this.player1) !== -1 && (this.player1.length >= 3) && $.inArray(win[i], this.player2) == -1) {
+          //   return alert('Player1 wins!');
+    // isDraw: function () {
+    //   var win = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [ 1, 4, 7], [2, 5, 8], [1, 4, 8], [2, 4, 6] ];
+    //     for (var i = 0; i < win.length; i++) {
+    //       if ( ($( win[i] ).not( this.player1 ).length !== 0) && ($( win[i] ).not( this.player2 ).length !== 0 ) ) {
+    //         return alert( 'This is a Draw!' );
+    //       }
+    //     }
+    // },
 
   	// playerMove: function () {
   	// 	var turnCount = 0;
