47ea63c3b682d9d7e3e799d5227803d3e4276297
djstozza <daniel.sztolcman@gmail.com>
2015-08-21 03:20:03 +1000

    add sound effects and some css

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -19,43 +19,96 @@ var ticTacToe = {
     player1Wins: 0,
     player2Wins: 0,
     gameCount: 0,
+    turnCount: 0,
+    winner: '',
 
-  	
+  	welcome: function(){
+      $('button.mario').click(function () {
+        $('body').addClass('fun');
+        marioStart.play();
+        ticTacToe.funImages();
+        $('.hello').css('display', 'none');
+        ticTacToe.clearBoard();
+        
+       });
+      $('button.normal').click(function () {
+        $('.hello').css('display', 'none');
+        ticTacToe.clearBoard();
+        $('body').removeClass('fun');
+          
 
+      })
+    },
+
+    funImages: function () {
+      if ($('body').hasClass('fun')) {
+        $('button.cell').click(function(){
+        $('span').text('');
+
+        if (ticTacToe.turnCount%2!==0) {
+            $(this).find('img.X').show();
+            mario.play();
+          } else {
+            $(this).find('img.O').show();
+            bowser.play();
+          }
+        });
+      }
+    },
+
+
+    playWinningAudio: function () {
+      if ( $("body").hasClass("fun") ) {
+        console.log(ticTacToe.winner);
+        
+
+        if (ticTacToe.winner === 'player1') {
+          console.log(ticTacToe.winner);
+          winner.play();
+        } else
+        if (ticTacToe.winner === 'player2'){
+          lose.play();
+        } else {
+          noWin.play();
+        }
+
+        
+        
+      }
+    },
   	
   	winningMove: function () {
   		var win = [	[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [ 1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]	];
         for (var i = 0; i < win.length; i++) {
           
-          // if ( ($( win[i] ).not( this.player1 ).length !== 0) && (this.player1.length + this.player2.length === 9) && ($( win[i] ).not( this.player2).length !== 0)) {
-            
-
-          //   $('playAgain h1').text('This is a draw!');
-          //   $('.playAgain').css('opacity', '1');
-          //   return;
-          // } else
 
           if ( $( win[i] ).not( this.player1 ).length === 0 && (this.player1.length >= 3)) {
             
+            ticTacToe.winner = 'player1';
+
             this.player1Wins++;
             $('.playAgain h1').text('Player1 Wins!');
             $('.moveBlock').css('display', 'inline-block');
             $('.playAgain').css('display', 'inline-block');
+            ticTacToe.playWinningAudio();
             return;
             
           } else if ( $( win[i] ).not( this.player2 ).length === 0 && (this.player2.length >= 3) ) {
             
+            ticTacToe.winner = 'player2';
             this.player2Wins++;
             $('.playAgain h1').text('Player2 Wins!');
             $('.moveBlock').css('display', 'inline-block');
             $('.playAgain').css('display', 'inline-block');
-            
+            ticTacToe.playWinningAudio();
             return;
 
-          } else if (!($( win[i] ).not( this.player1 ).length === 0) && (this.player1.length ===5 ) ) {
+          } else if (!($( win[i] ).not( this.player1 ).length === 0) && (this.player1.length ===5 ) || !($( win[i] ).not( this.player2 ).length === 0) && (this.player2.length ===5 )) {
             $('.playAgain h1').text('This is a draw!');
             $('.moveBlock').css('display', 'inline-block');
             $('.playAgain').css('display', 'inline-block');
+            ticTacToe.winner = 'draw';
+            ticTacToe.playWinningAudio();
             return;
           }
           
@@ -68,9 +121,12 @@ var ticTacToe = {
          
 
     clearBoard: function () {
-      $('button.cell').text('');
+      $('span').text('');
       $('playAgain h1').text('');
-      $('.moveBlock').css('display', 'none')
+      $('.moveBlock').css('display', 'none');
+      $('img').css('display', 'none');
+
+      this.winner = '';
       this.board = [ null, null, null, null, null, null, null, null, null ];
       this.player1 = [];
       this.player2 = [];
@@ -89,34 +145,40 @@ var ticTacToe = {
 
   	init: function () {
     // jQuery stuff goes here
-      var turnCount = 0;
+      ticTacToe.welcome();
       $('.player1Score .points').text(ticTacToe.player1Wins);
       $('.player2Score .points').text(ticTacToe.player2Wins);
+      
       $('button.cell').on('click', function(){
-      var id = parseInt( $(this).attr('id') );
-    
+        var id = parseInt( $(this).attr('id') );
+
     //stops a square from being re-assigned when it has already been clicked
     //.trim() to make sure there is no blank space in the square already
-      if ($(this).text().trim() !== ''){
+        if ($(this).find("img.O").css("display") !== 'none' || $(this).find("img.X").css("display") !== "none"){
           return;
-    }
-      //.attr()will find the attribute
-      if (turnCount%2 === 0) {
-        $(this).text('X').addClass('X');
-        ticTacToe.player1.push(id);
-        ticTacToe.player1.sort();
-        ticTacToe.board[id] = "X";
-        
-      } else {
-        
-        ticTacToe.player2.push(id);
-        ticTacToe.player2.sort();
-        $(this).text('O').addClass('O');
-        ticTacToe.board[id] = "O";
-      }
+        }
+        if ($(this).text().trim() !== ''){
+          return;
+        }
+
+        //.attr()will find the attribute
+        if (ticTacToe.turnCount % 2 === 0) {
+          //put the text in a span so that it isn't the only child in the td
+          $(this).find('span').text('X');
+          ticTacToe.player1.push(id);
+          ticTacToe.player1.sort();
+          ticTacToe.board[id] = "X";
+          
+        } else {
+          ticTacToe.funImages();
+          ticTacToe.player2.push(id);
+          ticTacToe.player2.sort();
+          $(this).find('span').text('O');
+          ticTacToe.board[id] = "O";
+        }
       
-      ticTacToe.winningMove();
-      turnCount++;
+        ticTacToe.winningMove();
+        ticTacToe.turnCount++;
 
     
     });
@@ -127,7 +189,9 @@ var ticTacToe = {
     })
 
     $('button.no').on('click', function () {
-      $('.goodbye').css('display', 'inline-block');
+      $('.hello').show();
+      $('.playAgain').css('display', 'none');
+
 
     });
   }
